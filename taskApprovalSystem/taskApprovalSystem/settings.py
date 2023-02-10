686e9507c641290f4ac327c4a751b2169815bbb1
hercules <aashish2535@gmail.com>
2023-02-11 00:07:17 +0530

    fixed secret_key exposure

--- taskApprovalSystem/taskApprovalSystem/settings.py
+++ taskApprovalSystem/taskApprovalSystem/settings.py
@@ -9,24 +9,24 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv,find_dotenv
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uj)ipr27=1yjdjhk++a9(ydpjn$e^8_mz_kwpk8b&6!ok41(3x'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,17 +69,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'taskApprovalSystem.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'taskapprovaldb',
+        'USER': 'hercules',
+        'PASSWORD': 'heydb',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -99,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -110,7 +111,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
--- taskApprovalSystem/taskApprovalapp/views.py
+++ taskApprovalSystem/taskApprovalapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
+
 
 def index(request):
     return HttpResponse("hello you are in the app")
