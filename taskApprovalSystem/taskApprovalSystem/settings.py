12bb93ae2a41a73aee3b7000ba9293908ac9c02d
hercules <aashish2535@gmail.com>
2023-02-14 02:30:05 +0530

    configured db and admin

--- taskApprovalSystem/taskApprovalSystem/settings.py
+++ taskApprovalSystem/taskApprovalSystem/settings.py
@@ -11,18 +11,24 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-from dotenv import load_dotenv,find_dotenv
-load_dotenv(find_dotenv())
+import dotenv
+
+# from dotenv import load_dotenv, find_dotenv
+
+# load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -31,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'taskApprovalapp.apps.TaskapprovalappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
